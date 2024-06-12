dbd51a4863506ff968618bbbceb206b703460678
Maxim <maximagaffonnovv@gmail.com>
2024-06-12 17:48:22 +0300

    add

--- src/components/reload/member1.js
+++ src/components/reload/member1.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setMember1} from '../../store/slice/slice';
 import { getDatabase, ref, onValue } from 'firebase/database';
-// import addNotification from 'react-push-notification';
 
 
 export const Member1Subscriber = () => {
@@ -13,10 +12,6 @@ export const Member1Subscriber = () => {
     const unsubscribe = onValue(dataRef, (snapshot) => {
       const data = snapshot.val();
       dispatch(setMember1(data));
-      // addNotification({
-      //   title: 'Warning',
-      //   native:true        
-      // })
     });
 
     return () => unsubscribe();

5414ed9797280af8e2b895765ef7912a8083dfff
Maxim <maximagaffonnovv@gmail.com>
2024-06-12 23:00:07 +0300

    add

--- src/components/reload/member3.js
+++ src/components/reload/member3.js
@@ -2,10 +2,10 @@ import { useEffect, useRef  } from 'react';
 import { useDispatch } from 'react-redux';
 import { setMember3} from '../../store/slice/slice';
 import { getDatabase, ref, onValue } from 'firebase/database';
-import { useSelector } from "react-redux";
+
 
 export const Member3Subscriber = () => {
-  const members = useSelector((state) => state.table.nameMembers);
+
   const dataLengthRef2 = useRef(0);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -13,7 +13,7 @@ export const Member3Subscriber = () => {
     const dataRef = ref(db, 'member3/');
     const unsubscribe = onValue(dataRef, (snapshot) => {
       const data = snapshot.val();
-      const newDataLength = data?.length || 0;
+      const newDataLength = data?.length || -1;
       dispatch(setMember3(data));
       if (newDataLength > dataLengthRef2.current) {
         console.log(newDataLength);
@@ -22,7 +22,6 @@ export const Member3Subscriber = () => {
             new Notification("Добавлено новое проклятие");
         } else if (dataLengthRef2.current !== 0) {
           new Notification("Добавлено новое проклятие");
-          console.log(members[2]);
         }
       }
       dataLengthRef2.current = newDataLength;

0dbcee16ffdaa0fc5ebd896e316e8c5bf99873c1
Maxim <maximagaffonnovv@gmail.com>
2024-06-12 11:03:59 +0300

    add

--- src/routes.js
+++ src/routes.js
@@ -26,7 +26,18 @@ export const AppRoutes = () => {
     dispatch(setNameMembers(names));
 
   };
-
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Успешная регистрация
+        console.log('ServiceWorker registration successful');
+      }, function(err) {
+        // При регистрации произошла ошибка
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
   useEffect(() => {
     handleGetData()
   }, []);

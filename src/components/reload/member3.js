b060819881af80e740adabec1377d88f6356a8c1
Maxim <maximagaffonnovv@gmail.com>
2024-06-12 22:25:52 +0300

    add

--- src/components/reload/member3.js
+++ src/components/reload/member3.js
@@ -1,9 +1,11 @@
-import { useEffect, useRef  } from 'react';
+import { useEffect, useRef,useState  } from 'react';
 import { useDispatch } from 'react-redux';
 import { setMember3} from '../../store/slice/slice';
 import { getDatabase, ref, onValue } from 'firebase/database';
 
 export const Member3Subscriber = ({name, userName}) => {
+  const [name1, setName1] = useState(name);
+  const [name2, setName2] = useState(userName);
   const dataLengthRef2 = useRef(0);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -15,7 +17,7 @@ export const Member3Subscriber = ({name, userName}) => {
       dispatch(setMember3(data));
       if (newDataLength > dataLengthRef2.current) {
         if (dataLengthRef2.current !== 0) {
-          if (String(name) === String(userName)) {
+          if (name1 === name2) {
             console.log("yes");
             new Notification("Добавлено новое проклятие");
           } else {

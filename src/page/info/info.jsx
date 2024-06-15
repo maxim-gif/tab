3b54b5cc3558af89429f5961a2cc9b84ac90df0c
Maxim <maximagaffonnovv@gmail.com>
2024-06-16 00:24:01 +0300

    add

--- src/page/info/info.jsx
+++ src/page/info/info.jsx
@@ -2,6 +2,7 @@ import './info.css';
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { CursesSubscriber } from '../../components/reload/curses';
 
 
 
@@ -9,10 +10,16 @@ export const Info = () => {
   const navigate = useNavigate();
     let curses = useSelector((state) => state.table.curses);
     const [search, setSearch] = useState("");
+    const [chance, setChance] = useState();
+    const [generalChance, setGeneralChance] = useState();
     const [searchResult, setSearchResult] = useState();
 
     useEffect(() => {
       setSearchResult(curses)
+      const chanceCurse = 100 / curses.filter(item => !item.general).length
+      const chanceGeneralCurse = 100 / curses.filter(item => item.general).length
+      setChance(chanceCurse.toFixed(2))
+      setGeneralChance(chanceGeneralCurse.toFixed(2))
     }, [curses]);
 
     useEffect(() => {
@@ -22,6 +29,7 @@ export const Info = () => {
 
   return (
     <div className="info">
+      <CursesSubscriber/>
       <div className="headerInfo">
         <div className="logoInfo"></div>
         <input className="searchInfo" type='text' value={search} onChange={(e) => {setSearch(e.target.value)}}></input>
@@ -33,6 +41,7 @@ export const Info = () => {
                       <img className="itemImg" src="/bgCurse.png" alt=''></img>
                         <h2>{item.name}</h2>
                         <span>{item.title}</span>
+                        <h3>{item.general ? `Общее проклятие. Шанс ${generalChance}%`:`Шанс выпадения ${chance}%`}</h3>
                     </div>)))
                 }
             </div>

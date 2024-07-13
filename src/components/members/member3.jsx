62818ed5efc7a4edb1621983bf2ea389b580af26
Maxim <maximagaffonnovv@gmail.com>
2024-07-13 12:45:14 +0300

    add

--- src/components/members/member3.jsx
+++ src/components/members/member3.jsx
@@ -39,7 +39,7 @@ export const Member3 = ({ moderatorsAccess, superModeratorsAccess, name }) => {
       addCurse(2, newData);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataUncompleted3,dataMember3]);
+  }, [dataMember3]);
 
   
   const handleAddUncompleted = (curse) => {
